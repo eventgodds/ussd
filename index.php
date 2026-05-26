e4be83f6f0bc2e26a0aa9cb1a7b772cd88611cf8
NanaKojo2018 <55985283+NanaKojo2018@users.noreply.github.com>
2026-05-26 12:56:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
-
-header("Content-Type: application/json");
-
-$response = "Welcome to Ghartey Event\n";
-$response .= "1. Vote";
-
 echo json_encode([
-    "response" => $response,
-    "continueSession" => true
+  "response" => "Welcome to Ghartey Event",
+  "continueSession" => true
 ]);
-
 ?>
